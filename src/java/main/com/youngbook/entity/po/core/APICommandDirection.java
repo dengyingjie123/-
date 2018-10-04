55d2f1fc49140f4130f9fe82e35f40099e614d77
leevits <lee@youngbook.com>
2018-10-04 17:16:04 +0800

    完善代码，编译不过 4

---
+++ src/java/main/com/youngbook/entity/po/core/APICommandDirection.java
@@ -0,0 +1,10 @@
+package com.youngbook.entity.po.core;
+
+/**
+ * Created by leevits on 7/22/2018.
+ */
+public class APICommandDirection {
+
+    public static int Send = 1;
+    public static int Receive = 2;
+}
