39d115570be2e148260bd3b9935d0464e8d30937
leevits <lee@youngbook.com>
2018-10-22 17:56:33 +0800

    实现加密解密

--- src/java/main/com/youngbook/common/utils/CryptographyUtils.java
+++ src/java/main/com/youngbook/common/utils/CryptographyUtils.java
@@ -4,13 +4,12 @@ import javax.crypto.Cipher;
 import java.io.FileInputStream;
 import java.security.*;
 import java.security.cert.*;
+import java.security.cert.Certificate;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Enumeration;
 
-/**
- * Created by leevits on 10/19/2018.
- */
+
 public class CryptographyUtils {
 
     private static final String ALGORITHM = "RSA";
@@ -43,6 +42,27 @@ public class CryptographyUtils {
         return decryptedBytes;
     }
 
+    public static KeyPair getKeyPair(KeyStore keystore, String alias, char[] password) {
+        try {
+            Key key = keystore.getKey(alias, password);
+            if (key instanceof PrivateKey) {
+                Certificate cert = keystore.getCertificate(alias);
+                PublicKey publicKey = cert.getPublicKey();
+                return new KeyPair(publicKey, (PrivateKey) key);
+            }
+
+            if (key instanceof PublicKey) {
+                Certificate cert = keystore.getCertificate(alias);
+                PublicKey publicKey = cert.getPublicKey();
+                return new KeyPair(publicKey, null);
+            }
+        } catch (UnrecoverableKeyException e) {
+        } catch (NoSuchAlgorithmException e) {
+        } catch (KeyStoreException e) {
+        }
+        return null;
+    }
+
     public static KeyPair generateKeyPair()
             throws NoSuchAlgorithmException, NoSuchProviderException {
 
@@ -72,25 +92,85 @@ public class CryptographyUtils {
         System.out.println(new String(decryptedData));
 
 
-        test();
+        // test();
+        testPublic();
 
     }
 
 
+
+
     public static void test () throws Exception {
 
+        String file_pfx = "C:\\Users\\leevits\\Desktop\\ttt\\p-sha1.pfx";
+        // KeyStore keyStore = KeyStore.getInstance("PKCS12");
+        String jks_pfx = "C:\\Users\\leevits\\Desktop\\ttt\\p-sha1.jks";
+
+        String alias = "kepler_private";
         String password = "12345678";
-        FileInputStream fis = new FileInputStream("C:\\Users\\leevits\\Desktop\\ttt\\p-sha1.pfx");
+        FileInputStream fis = new FileInputStream(jks_pfx);
 // Instantiate BouncyCastle KeyStore implementation
-        KeyStore keyStore = KeyStore.getInstance("PKCS12");
+        KeyStore keyStore = KeyStore.getInstance("JKS");
         keyStore.load(fis, password.toCharArray());
         fis.close();
+
+        Key key = keyStore.getKey(alias, password.toCharArray());
+
+        System.out.println(key);
+
         Enumeration aliases = keyStore.aliases();
         while(aliases.hasMoreElements()) {
             System.out.println(aliases.nextElement());
-
         }
 
+
+        KeyPair keyPair = getKeyPair(keyStore, alias, password.toCharArray());
+
+
+        byte[] publicKey = keyPair.getPublic().getEncoded();
+        byte[] privateKey = keyPair.getPrivate().getEncoded();
+
+        byte[] encryptedData = encrypt(publicKey,
+                "hi this is Visruth 22222".getBytes());
+
+        byte[] decryptedData = decrypt(privateKey, encryptedData);
+
+        System.out.println(new String(decryptedData));
+
+    }
+
+
+    public static void testPublic() throws Exception {
+
+        String jks_public = "C:\\Users\\leevits\\Desktop\\ttt\\kepler_public.jks";
+
+        String alias = "kepler_public";
+        String password = "12345678";
+        FileInputStream fis = new FileInputStream(jks_public);
+// Instantiate BouncyCastle KeyStore implementation
+        KeyStore keyStore = KeyStore.getInstance("JKS");
+        keyStore.load(fis, password.toCharArray());
+        fis.close();
+
+        Key key = keyStore.getKey(alias, password.toCharArray());
+
+        Certificate certificate = keyStore.getCertificate(alias);
+
+
+        Enumeration aliases = keyStore.aliases();
+        while(aliases.hasMoreElements()) {
+            System.out.println(aliases.nextElement());
+        }
+
+//        System.out.println(key);
+//
+//
+//        byte[] publicKey = key.getEncoded();
+//
+//        byte[] encryptedData = encrypt(publicKey,
+//                "hi this is Visruth 22222".getBytes());
+//
+//        System.out.println(new String(encryptedData));
 
     }
 }
