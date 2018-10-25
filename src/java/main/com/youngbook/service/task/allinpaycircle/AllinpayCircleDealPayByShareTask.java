7bafa9790440b7d194142c4dc487fc6928c997a6
leevits <lee@youngbook.com>
2018-10-25 16:33:00 +0800

    修补代码

---
+++ src/java/main/com/youngbook/service/task/allinpaycircle/AllinpayCircleDealPayByShareTask.java
@@ -0,0 +1,64 @@
+package com.youngbook.service.task.allinpaycircle;
+
+import com.youngbook.common.config.Config;
+import com.youngbook.common.utils.runner.Task;
+import com.youngbook.service.allinpaycircle.AllinpayCircleService;
+import com.youngbook.service.system.LogService;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Created by leevits on 9/1/2018.
+ */
+public class AllinpayCircleDealPayByShareTask extends Task {
+
+    AllinpayCircleService allinpayCircleService = Config.getBeanByName("allinpayCircleService", AllinpayCircleService.class);
+
+    public AllinpayCircleDealPayByShareTask() {
+
+        this.setName("AllinpayCircleDealPayByShareTask");
+
+        this.setStartTime("2010-02-08 00:00:0");
+        this.setStopTime("2020-02-08 00:00:0");
+        this.setRepeatSecond(10);
+    }
+
+    @Override
+    public void run() {
+
+        LogService.console("AllinpayCircleDealPayByShareTask running - Start");
+        Connection conn = null;
+
+
+        try {
+            conn = Config.getConnection();
+
+            conn.setAutoCommit(false);
+
+            allinpayCircleService.dealPayByShare(conn);
+
+            conn.commit();
+        }
+        catch (Exception e) {
+            try {
+                if (conn != null) {
+                    conn.rollback();
+                }
+
+            } catch (Exception e1) {
+                e1.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+            }
+        }
+    }
+}
