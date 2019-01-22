1022f81c2821ddb547db85118728985825b79a33
huchaoyi <huchaoyi@keplerinfo.net>
2019-01-22 15:36:14 +0800

    跳转接口

--- src/java/main/com/youngbook/dao/cms/IArticleDao.java
+++ src/java/main/com/youngbook/dao/cms/IArticleDao.java
@@ -14,4 +14,6 @@ public interface IArticleDao {
     public List<ArticleVO> getListArticleVO(ArticleVO articleVO,  Connection conn) throws Exception;
     public ArticlePO loadByArticleId(String articleId, Connection conn) throws Exception;
     public Pager getPagerArticleVO(ArticleVO articleVO, int currentPage, int showRowCount, Connection conn) throws Exception;
+
+    List<ArticlePO> getArticlePO(ArticlePO articlePO, Connection conn) throws Exception;
 }
